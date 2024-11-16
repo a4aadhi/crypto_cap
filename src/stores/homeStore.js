a4494520d39b4758d876649b74cbe2c3707ae97f
a4aadarsh1999@gmail.com <a4aadarsh1999@gmail.com>
2024-11-16 11:09:10 -0500

    cached api calls

--- src/stores/homeStore.js
+++ src/stores/homeStore.js
@@ -1,32 +1,47 @@
 import axios from "axios";
 import { create } from "zustand";
 
-const homeStore = create((set) => ({
+const homeStore = create((set, get) => ({
   coins: [],
   market: [],
+  lastFetchedCoins: null, // Timestamp for the last coins fetch
+  lastFetchedMarket: null, // Timestamp for the last market fetch
   fetchCoins: async () => {
-    try {
-      const res = await axios.get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=4&page=1&sparkline=false"
-      );
-      //console.log('Fetched coins:', res.data);
-      set({ coins: res.data });
-    } catch (error) {
-      console.error("Error fetching coins", error);
+    const { lastFetchedCoins, coins } = get();
+    const currentTime = Date.now();
+
+    // Only fetch if the last fetch was more than 5 minutes ago
+    if (!lastFetchedCoins || currentTime - lastFetchedCoins > 5 * 60 * 1000) {
+      try {
+        const res = await axios.get(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=4&page=1&sparkline=false"
+        );
+        set({ coins: res.data, lastFetchedCoins: currentTime });
+      } catch (error) {
+        console.error("Error fetching coins", error);
+      }
+    } else {
+      console.log("Using cached coins data", coins);
     }
   },
   fetchMarket: async () => {
-    try {
-      const res = await axios.get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad"
-      );
+    const { lastFetchedMarket, market } = get();
+    const currentTime = Date.now();
 
-      console.log("Fetched market status:", res.status);
-      console.log("Fetched market:", res.data);
-
-      set({ market: res.data });
-    } catch (error) {
-      console.error("Error fetching market", error);
+    // Only fetch if the last fetch was more than 5 minutes ago
+    if (!lastFetchedMarket || currentTime - lastFetchedMarket > 5 * 60 * 1000) {
+      try {
+        const res = await axios.get(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad"
+        );
+        console.log("Fetched market status:", res.status);
+        console.log("Fetched market:", res.data);
+        set({ market: res.data, lastFetchedMarket: currentTime });
+      } catch (error) {
+        console.error("Error fetching market", error);
+      }
+    } else {
+      console.log("Using cached market data", market);
     }
   },
 }));
